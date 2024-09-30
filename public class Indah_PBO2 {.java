19087bc7cc815b2c672db181ff974ea0859555a2
indhchya <indahcahya826@gmail.com>
2024-09-30 17:10:02 +0700

    Add files via upload

---
+++ public class Indah_PBO2 {.java
@@ -0,0 +1,14 @@
+public class Indah_PBO2 {
+    public static void main(String[] args) {
+        String nama = "Indah Cahya Ramadhani";
+        String npm = "2340506070";
+        String alamat = "Mejing 2 Candimulyo Kabupaten Magelang";
+        String nohp = "089345678321";
+
+        // Menampilkan informasi ke layar //
+        System.out.println("- Nama : " + nama);
+        System.out.println("- NPM : " + npm);
+        System.out.println("- Alamat : " + alamat);
+        System.out.println("- No Hp : " + nohp);
+    }
+}
